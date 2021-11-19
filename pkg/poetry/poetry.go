7fc7fc950a7bf5ede60e8b1b71c0eed150ba4dff
Tom <tomfleet2018@gmail.com>
2021-11-19 19:13:35 +0000

    Add a function to determine whether pyproject.toml is poetry

--- pkg/poetry/poetry.go
+++ pkg/poetry/poetry.go
@@ -5,9 +5,22 @@ import (
 	"fmt"
 	"io"
 	"os/exec"
+
+	"github.com/pelletier/go-toml/v2"
+	"github.com/spf13/afero"
 )
 
 var poetryCommand = exec.Command
+
+// If the build-backend says this, it's a valid poetry project
+const poetryMarker = "poetry.core.masonry.api"
+
+type pyProjectTOML struct {
+	BuildSystem struct {
+		Requires     []string `toml:"requires"`
+		BuildBackend string   `toml:"build-backend"`
+	} `toml:"build-system"`
+}
 
 // newPoetryCmd returns an exec.Cmd configured with the parameters passed in
 func newPoetryCommand(cwd string, stdout, stderr io.Writer, args []string) *exec.Cmd {
@@ -27,4 +40,21 @@ func Install(cwd string, stdout, stderr io.Writer) error {
 	}
 
 	return nil
+}
+
+// IsPoetryFile reads the contents of the toml file given by 'path' and
+// determines if this is a valid poetry pyproject.toml file
+func IsPoetryFile(af afero.Afero, path string) (bool, error) {
+	var pyToml pyProjectTOML
+
+	data, err := af.ReadFile(path)
+	if err != nil {
+		return false, fmt.Errorf("could not read %s: %w", path, err)
+	}
+
+	if err := toml.Unmarshal(data, &pyToml); err != nil {
+		return false, fmt.Errorf("could not unmarshall toml data: %w", err)
+	}
+
+	return pyToml.BuildSystem.BuildBackend == poetryMarker, nil
 }
